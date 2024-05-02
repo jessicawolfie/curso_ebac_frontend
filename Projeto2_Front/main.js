2add720083ad5e2b877202f47ed848cbc06c87a0
Jessica Wolfie <jessica.cafezeiro@gmail.com>
2024-05-02 15:09:15 -0300

    alteraçao no js

--- Projeto2_Front/main.js
+++ Projeto2_Front/main.js
@@ -1,7 +1,48 @@
-const form = document.getElementById('form-atividade');
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+const form = document.getElementById('form-atividade')
+const imgAprovado = '<img src="./images/aprovado.png" alt= "emoji celebrando"/>'
+const imgReprovado = '<img src="./images/reprovado.png" alt= "emoji decepcionado"/>'
+const atividades = [] //array vazio
+const notas = []
 
-    const inputNomeAtividade = document.getElementById('nome-atividade'); 
-    const inputNotaAtividade = document.getElementById('nota-atividade');
+let linhas = '' //string vazia
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    adicionaLinha()
+    atualizaTabela () 
+    atualizaMediaFinal () //chamar função
 })
+
+function adicionaLinha() { 
+const inputNomeAtividade = document.getElementById('nome-atividade')
+const inputNotaAtividade = document.getElementById('nota-atividade')
+
+atividades.push(inputNomeAtividade.value)
+notas.push(parseFloat(inputNotaAtividade.value))
+
+let linha = '<tr>' //abrir 
+linha += `<td>${inputNomeAtividade.value}</td>`
+linha += `<td>${inputNotaAtividade.value}</td>`
+linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>` //?interrogação = if : dois pontos = else 
+linha += `</tr>` //fechar 11
+
+linhas += linha
+
+inputNomeAtividade.value = ''
+inputNotaAtividade.value = ''
+}
+
+function atualizaTabela (){
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
+
+function atualizaMediaFinal (){
+    let somaDasNotas = 0
+
+    for(let i =0; 1 < notas.length; i++){
+        somaDasNotas += notas[i]
+    }
+    console.log(somaDasNotas)
+}
